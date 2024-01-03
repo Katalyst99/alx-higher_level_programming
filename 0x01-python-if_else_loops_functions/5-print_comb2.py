6d21db2b455b81abe1316b62a6bddc64849aad78
Katalyst99 <katleho333@gmail.com>
2024-01-02 17:41:40 -0800

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0,100):
     if number == 99:
-        print("{:d}". format(number))
+        print(number)
     else:
         print(f"{number:02}", end=', ')
