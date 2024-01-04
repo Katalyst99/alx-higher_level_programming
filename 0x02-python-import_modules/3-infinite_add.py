73e574516e96387489cb8f07c1113a47b4fb6055
Katalyst99 <katleho333@gmail.com>
2024-01-04 15:29:03 -0800

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for i in range(len(sys.argv[1:])):
+        result = result + int(sys.argv[i + 1])
+    print(result)
+
