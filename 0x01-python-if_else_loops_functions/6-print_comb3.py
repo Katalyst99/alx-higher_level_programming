fd8fe938be4654cd524ef442c625944c44071d15
Katalyst99 <katleho333@gmail.com>
2024-01-07 07:30:32 -0800

    Code fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for digit1 in range(0, 9):
     for digit2 in range(digit1 + 1, 10):
-        if f"{digit1}{digit2}" == "89":
-            print(f"{digit1}{digit2}")
+        if digit1 == 8 and digit2 == 9:
+            print('{:d}'.format(89),)
         else:
-            print(f"{digit1}{digit2}", end=", ")
+            print('{:d}{:d}'.format(digit1, digit2), end=', ')
