5799edc95ff0556ae776336b03d92d1b782d60f9
Katalyst99 <katleho333@gmail.com>
2024-01-08 04:12:04 -0800

    Fix errrors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copylist = my_list
+    copylist = my_list[:]
     if idx < 0 or  idx >= len(my_list):
-        return copylist
+        return my_list
     else:
         copylist[idx] = element
         return copylist
