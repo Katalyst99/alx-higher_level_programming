ecae4648d6d54511f79e3870c08a76cdcb5e9ae7
Katalyst99 <katleho333@gmail.com>
2024-01-10 11:05:10 -0800

    Error fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    m_keys = list(a_dictionary.keys())
-
-    for k in m_keys:
-        new_dict[k]= a_dictionary[k] * 2
-    return new_dict
+    return ({key: (value*2) for key, value in a_dictionary.items()})
