9392948047986bb1ce1f90ef26f5194397136a0c
Katalyst99 <katleho333@gmail.com>
2024-04-12 10:00:06 -0700

    Resolving errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,17 +2,16 @@
 """Defines the add integer function"""
 
 def add_integer(a, b=98):
-    """Returns an integer: the addition of a and b"""
+    """Returns an integer: the addition of a and b
 
 
-    """
     Args:
         a (int): first number
         b (int): second number
     """
-    if type(a) is not int and type(a) is not float or a is not a:
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    elif type(b) is not int and type(b) is not float or b is not b:
+    elif type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
     else:
         return int(a) + int(b)
