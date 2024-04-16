0ed90ab5849889ad6d77efbfcb761c83c7c16062
Katalyst99 <katleho333@gmail.com>
2024-04-16 03:11:19 -0700

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,8 +9,5 @@ def write_file(filename="", text=""):
     Args:
         filename (str): The file name
     """
-    char_number = 0
     with open(filename, 'w', encoding='utf-8') as afile:
-        for line in afile:
-            char_number += 1
-        return char_number
+        return afile.write(text)
