12dd0982cff17deb5bfa16d7d51467b5d3c7be86
Katalyst99 <katleho333@gmail.com>
2024-06-18 06:47:58 -0700

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myArg = process.argv.slice(2);
+const num = parseInt(myArg[0], 10);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
