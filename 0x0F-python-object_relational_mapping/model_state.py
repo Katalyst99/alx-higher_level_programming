ee478021b8f38004f3a6c63c5505cb03ec0708e0
Katalyst99 <katleho333@gmail.com>
2024-06-21 03:57:23 -0700

    Indentation fixes2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,10 +6,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """The class State defined"""
 
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, autoincrement=True,
                 unique=True, nullable=False)
     name = Column(String(128), nullable=False)
