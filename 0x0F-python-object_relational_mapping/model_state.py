30e5bc771bbd776ad84b8a9d7abfccbf945a6a3a
Katalyst99 <katleho333@gmail.com>
2024-06-21 03:44:54 -0700

    Indentation fixes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,5 +11,5 @@ class State(Base):
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True,
-                 unique=True, nullable=False)
-    name =  Column(String(128), nullable=False)
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
