6457271ab97915a99b0d873885fbc7a132765bd6
Katalyst99 <katleho333@gmail.com>
2024-07-05 10:57:11 -0700

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+if __name__ == "__main__":
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print('\t- type: {}'.format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
