4569398d703974ab165eb7970f2e65269163e2af
Katalyst99 <katleho333@gmail.com>
2024-08-07 15:14:58 -0700

    Fixed casing

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
 // A script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header
 $('DIV#red_header').click(function () {
-  $('Header').css('color', '#FF0000');
+  $('header').css('color', '#FF0000');
 });
