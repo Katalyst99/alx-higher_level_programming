31913c54ee4b7d724de721b54395c306680e7886
Katalyst99 <katleho333@gmail.com>
2024-08-07 16:38:46 -0700

    8. Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 // A script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
-$.getJSON('https://swapi-api.alx-tools.com/api/people/5/?format=json', data => {
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', data => {
   $.each(data.results, (i, film) => {
     $('UL#list_movies').append(`<li>${film.title}</li>`);
   });
